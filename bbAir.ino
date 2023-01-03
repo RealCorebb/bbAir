23be0733376b29b01014665fbb4a8233bde45a39
RealCorebb <coreoobb@qq.com>
2023-01-03 11:43:52 +0800

    BLE update Credentials

--- bbAir.ino
+++ bbAir.ino
@@ -1,32 +1,95 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <Preferences.h>
+#include <WiFi.h>
+
+Preferences preferences;
+
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
-
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic *pCharacteristic) {
-      std::string value = pCharacteristic->getValue();
-
-      if (value.length() > 0) {
-        Serial.println("*********");
-        Serial.print("New value: ");
-        for (int i = 0; i < value.length(); i++)
-          Serial.print(value[i]);
-
-        Serial.println();
-        Serial.println("*********");
-      }
-    }
-};
-
-
 #define pumpTime 40
 
 #define OUT1 4
 #define OUT2 5
 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.println("OUT1 HIGH");
+  digitalWrite(OUT1,HIGH);
+  delay(pumpTime);
+  //Serial.println("OUT1 LOW");
+  digitalWrite(OUT1,LOW);  
+
+  for(int i = 0;i<3;i++){
+    digitalWrite(OUT2,HIGH);
+    delay(20);
+    digitalWrite(OUT2,LOW);
+digitalWrite(OUT1,HIGH);    
+    delay(pumpTime);
+digitalWrite(OUT1,LOW);  
+delay(200);    
+  }
+  
+  
+  delay(2000);
+  
+}
+
+void setupWifi(){
+  String ssid = preferences.getString("ssid","null");
+  String passwd = preferences.getString("passwd","null");
+  Serial.print("WIFI:");
+  Serial.print(ssid);
+  Serial.print(" ");
+  Serial.println(passwd);
+  //if ssid and passwd not null
+  if(ssid != "null" && passwd != "null"){
+    //convert String to char array
+    WiFi.begin(ssid.c_str(),passwd.c_str());
+  }
+}
+
+void newCredentials(String ssid,String passwd){
+  preferences.putString("ssid",ssid);
+  preferences.putString("passwd",passwd);
+}
+
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      String ssid = "";
+      String passwd = "";
+      bool passFlag = false;
+      if (value.length() > 0) {
+        Serial.println("*********");
+        Serial.print("New value: ");
+        for (int i = 0; i < value.length(); i++){
+          Serial.print(value[i]);
+          if(value[i] != ' '){
+            if(!passFlag)
+              ssid += value[i];
+            else
+              passwd += value[i];
+          }
+          else passFlag = true;
+        }
+
+        Serial.println();
+        Serial.println("*********");
+
+        newCredentials(ssid,passwd);
+        setupWifi();
+        pCharacteristic->setValue("");
+      }
+    }
+};
+
+
 void setup() {
+  preferences.begin("bbAir", false);
   // put your setup code here, to run once:
   pinMode(OUT1,OUTPUT);
   pinMode(OUT2,OUTPUT);
@@ -49,29 +112,10 @@ void setup() {
   pService->start();
 
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
+  pAdvertising->start();  
   //BLE
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("OUT1 HIGH");
-  digitalWrite(OUT1,HIGH);
-  delay(pumpTime);
-  Serial.println("OUT1 LOW");
-  digitalWrite(OUT1,LOW);  
-
-  for(int i = 0;i<3;i++){
-    digitalWrite(OUT2,HIGH);
-    delay(20);
-    digitalWrite(OUT2,LOW);
-digitalWrite(OUT1,HIGH);    
-    delay(pumpTime);
-digitalWrite(OUT1,LOW);  
-delay(200);    
-  }
-  
-  
-  delay(2000);
-  
+  //WiFi -_,-
+  setupWifi();
+  //WiFi
 }
